fcbfddd5dc6781aec451aacb04b6f74200a4fd9a
caosytrung <caotrung.kk@gmail.com>
2016-06-17 17:27:43 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import com.trungcs.goldrush.GUI;
+
+/**
+ * Created by caotr on 10/06/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        (new GUI()).setVisible(true);
+    }
+}
